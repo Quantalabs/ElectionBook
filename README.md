ab37e8cec85afdad22fd7fbe3e7856f251304422
QLabs <55121845+Quantalabs@users.noreply.github.com>
2024-04-03 18:50:42 -0700

    feat: :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ElectionBook
+Using social media to look at political trends across the nation.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>ElectionBook</h1>
+<p>Using social media to look at political trends across the nation.</p>
